3bd2e9b48e35d78066e57d5c5dc20573f2b6433b
saleemgrich <saleemagrich@gmail.com>
2025-04-04 19:55:25 -0400

    fixed import error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 // src/pages/_app.tsx
 import type { AppProps } from 'next/app'
-import Header from '@/components/Header'
+import Header from '../components/Header'
 import '../styles/global.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
