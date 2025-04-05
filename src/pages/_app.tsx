96e1d3abdad03766165c32ac5305c2520c4d30c1
saleemgrich <saleemagrich@gmail.com>
2025-04-04 20:11:17 -0400

    added nav bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 // src/pages/_app.tsx
 import type { AppProps } from 'next/app'
 import Header from '../components/Header'
+import Navigation from '../components/Header'
+
 import '../styles/global.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
             <Header />
+            <Navigation />
             <Component {...pageProps} />
         </>
     )
