4b3ccd7a06f892ef1f1081eaa570b6d45d519eb5
saleemgrich <saleemagrich@gmail.com>
2025-04-07 23:45:50 -0400

    fixing image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,19 @@ import Header from '../components/Header'
 import Navigation from '../components/Navigation'
 
 import '../styles/global.css'
+import Image from "next/image";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
             <Header />
-            <div className="logo-container">
-                <img src="/temp-hero-design.jpg" alt="Company Logo" className="center-logo" />
+            <div className="image-wrapper">
+                <Image
+                    src="/temp-hero-design.png"
+                    alt="Company Logo"
+                    fill
+                    style={{ objectFit: "contain" }}
+                />
             </div>
             <Navigation />
             <Component {...pageProps} />
